7126bf7b94a873b48d0c175679257728c2626260
Ryan Achten <ryanachten@gmail.com>
2026-08-09 07:57:18 +1200

    feat: add SNS integration and seeding

---
+++ README.md
@@ -0,0 +1,6 @@
+# ore
+
+## Development
+
+### Prerequisites
+- Ensure localstack script is executable: `chmod +x localstack/init.sh`
---
+++ localstack/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "----------- Initializing LocalStack Resources -----------"
+
+awslocal sns create-topic --name ore-sim
